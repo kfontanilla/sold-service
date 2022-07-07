70c773c6640e9563745d823c1bc1492af0de8f0a
Kevin Fontanilla <kfontanilla@placester.com>
2022-07-07 23:10:22 +0800

    ec2-deployment

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,7 +3,13 @@ const fs = require('fs');
 const path = require('path');
 
 const ENV: string = process.env.NODE_ENV || "local";
-const dbConfig = path.join(__dirname, '/database.ts');
+
+let fileExtension = '.ts'
+if (fs.existsSync(`${__dirname}/database.js`)) {
+  fileExtension = '.js'
+}
+
+const dbConfig = path.join(__dirname, `/database${fileExtension}`);
 
 function loadDbConfig() {
   if (fs.existsSync(dbConfig)) {
