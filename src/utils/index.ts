9cf43d52e7b930ffa2f0e616f86117c4c04d4470
Kevin Mike Fontanilla <220544.KFontanilla@gbs.local>
2022-07-22 15:05:25 +0800

    Image Helper Utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,9 @@
 const responseFormatter = require('./ResponseFormatter')
 const payloadHelper = require('./PayloadHelper')
+const imageHelper = require('./ImageHelper')
 
 export = {
   responseFormatter,
   payloadHelper,
+  imageHelper,
 }
